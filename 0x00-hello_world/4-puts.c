56f877296f443de23128ac1cfb6e3cad27177394
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-13 12:17:05 -0700

    puts commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *
+ *  *main- Entry point for the program
+ *
+ *   *
+ *
+ *    *Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
