99eaa99fe662c9aec498362c21e8859c67eba829
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-13 13:56:26 -0700

    a printf commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point of the program.
+ *  main - Entry point of the program.
  *   
- *  Return: By default returns zero if no erros otherwise returns non-zero value.
+ *  Return: Always 0.
  */
-	int main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
