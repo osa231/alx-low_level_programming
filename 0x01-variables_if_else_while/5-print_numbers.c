ef39561b2081b61a1564e8b2d3948f26229404d8
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-14 16:40:37 -0700

    numbers commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  * main - Entry point of program
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+	putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
