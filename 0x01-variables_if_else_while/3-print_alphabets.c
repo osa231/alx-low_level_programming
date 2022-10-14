68bbe6e3cd0510a0e422cf5408fbe607f26f4a0d
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-14 16:22:48 -0700

    alphabet commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+		int ch;
+
+		for (ch = 'a'; ch <= 'z'; ch++)
+
+		{
+
+	putchar(Ch);
+		}
+		for (ch = 'A'; ch <= 'Z'; ch++)
+		{
+
+		putchar(ch);
+		}
+		putchar('\n');
+
+		return (0);
+
+}
