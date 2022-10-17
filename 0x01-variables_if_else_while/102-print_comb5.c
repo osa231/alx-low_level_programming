e9ae83848bd07ea9e6cf0b81332d7c404be2eebc
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-17 04:12:01 -0700

    software eating commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,13 +5,14 @@
  * description: prints all possible combinations of two two-digit numbers
  * Return: Alawys 0 (success)
  */
+
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 99; j <= 99; j++)
+	for (i = '0'; i < 99; j <= '99'; j++)
 	{
-	for (j = i + 1; j <= 99; j++)
+	for (j = i + '1'; j <= 99; j++)
 	{
 		putchar('0' + i / 10);
 		putchar('0' + i % 10);
