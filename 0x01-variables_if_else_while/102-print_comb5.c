d21916f355426378fdbcef1b70975baab1282b28
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-17 04:31:42 -0700

    software eating commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 99;i++)
+	for (i = 0; i < 99; i++)
 	{
 	for (j = i + 1; j <= 99; j++)
 	{
@@ -20,7 +20,7 @@ int main(void)
 		putchar(' ');
 
 		putchar('0' + j / 10);
-		putchar('0' + j / % 10);
+		putchar('0' + j % 10);
 
 		if (i == 98 && j == 99)
 			break;
