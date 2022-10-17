289dcd179feb1c2f55391de789de63738ffc685d
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-17 03:28:09 -0700

    do better commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	int m, i, j
 
-	for (i = '0'; i <= '9'; i++)
+	for (i ='0'; i <= '9'; i++)
 	{
-	for (j = '0'; j <= '9'; j++)
+	for (j ='0'; j <= '9'; j++)
 	{
-	for (m = '0'; m <= '9'; m++)
+	for (m ='0'; m <= '9'; m++)
 	{
 	if (i < j && j < m)
 	{
