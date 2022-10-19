b641da9627a1e6b3adf87c0d85ecd89e2129d7cf
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-18 22:25:06 -0700

    absolute commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes abs value of int
+ * @n: int
+ * Return: abs value of int
+ */
+int _abs(int n);
+{
+	if (n >= 0)
+	return (n);
+	else
+	return (-n);
+	{
+	_putchar('\n');
+	}
+}
