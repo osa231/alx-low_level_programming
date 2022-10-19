fa0c2e48ca747f68ae14ff4b6bce6c0dddf01d38
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-18 21:09:22 -0700

    the lowercase commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * _islower - function to check c for lowercase character
+ * _islower - function to check for lowercase character
+ * @c: char
  * Return: 1 if char is lowercase, else 0
  */
-void _islower(void)
+int _islower(int c)
 {
 	int c;
 
