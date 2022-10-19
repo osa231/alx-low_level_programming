87952a8298e27c8f132ed4069da9011f28a2a8cf
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-19 07:42:11 -0700

    putchar commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN.H
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -9,4 +6,4 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 
-#endif
+
