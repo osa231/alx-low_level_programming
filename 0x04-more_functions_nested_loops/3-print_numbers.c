61fe3f00be76ca52483bebf77f30cea0b754722a
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-20 17:18:15 -0700

    print num commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+	putchar(ch);
+	}
+
+	putchar ('\n');
+}
