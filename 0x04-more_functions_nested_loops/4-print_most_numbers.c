c8f3f0a1c6c4dac82f7052b682f1dfab34213cd4
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-20 17:29:13 -0700

    most nums commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+	if (ch == '2' || ch == '4')
+	{
+		continue;
+	}
+	_putchar(ch);
+	}
+
+	_putchar ('\n');
+}
