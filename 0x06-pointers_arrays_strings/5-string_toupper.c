44f2a95cd63227118c512098f72658aad45d61c9
osa231 <osa_agbonwanegbe@yahoo.com>
2022-10-30 08:47:31 -0700

    look up commit

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,7 +6,7 @@
  * Return:char
  */
 
-char *string_toupper(char *s);
+char *string_toupper(char *s)
 
 {
 
@@ -16,7 +16,7 @@ i = 0;
 
 	while (*(s + i))
 
-	}
+	{
 
 	if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		*(s + i) -= 'a' - 'A';
